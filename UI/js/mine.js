6c04f263a22b3bede8bae8484177fa9a56ac3d4d
codexempire <princewillifeanyi1999@gmail.com>
2019-03-22 21:50:30 +0100

    ft-user-able-view-accounts-transaction-164705379 Created a page from which a user can view his/her transaction history and linked the login button to the users dashboard using javascript

--- UI/js/mine.js
+++ UI/js/mine.js
@@ -1,14 +1,7 @@
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+const startApp = () => {
+  const btn = document.getElementById('login');
+  btn.onclick = () => {
+    location.replace('profile.html');
   }
-  slideIndex++;
-  if (slideIndex > slides.length) { slideIndex = 1 }
-  slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+startApp();
