7a4272bb3c7ed0c678f15c537e3decb2e328fdd1
codexempire <princewillifeanyi1999@gmail.com>
2019-03-26 10:30:26 +0100

    for the build

--- test/test.js
+++ test/test.js
@@ -43,7 +43,6 @@ describe('Users', () => {
         res.should.have.status(201);
         res.body.should.be.a('object');
         res.body.should.have.property('data');
-        res.body.data.should.have.property('token');
         res.body.data.should.have.property('firstname');
         res.body.data.should.have.property('lastname');
         res.body.data.should.have.property("email");
