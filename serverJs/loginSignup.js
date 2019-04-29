bd6ea3f6e2a3e6501cf44d0e6a428310f0794f14
codexempire <princewillifeanyi1999@gmail.com>
2019-04-29 21:48:46 +0100

    still working

--- serverJs/loginSignup.js
+++ serverJs/loginSignup.js
@@ -47,35 +47,48 @@ class Signup {
   return;
  }
  validateFields() {
-  console.log(this.firstname.value);
-  if (!this.firstname.value) return 'Enter your Firstname';
-  if (this.firstname.value.trim().replace(/\s+/g, '').length < 3) return 'Firstname must  have at least 3 alphabets';
+  if (!this.firstname.value) {
+   return 'Enter your Firstname';
+  }
+  if (this.firstname.value.trim().replace(/\s+/g, '').length < 3) {
+   return 'Firstname must  have at least 3 alphabets';
+  }
   if (!this.lastname.value) return 'Enter your Lastname';
-  if (this.lastname.value.trim().replace(/\s+/g, '').length < 3) return 'Lastname must  have at least 3 alphabets';
-  if (!this.email.value) return 'Enter email';
+  if (this.lastname.value.trim().replace(/\s+/g, '').length < 3) {
+   return 'Lastname must  have at least 3 alphabets';
+  }
+  if (!this.email.value) {
+   return 'Enter email';
+  }
   // regex used for email validation gotten from stackoverflow
-  // if (!(/.+@[^@]+\.[^@]{2,}$/.test(this.email))) return 'Enter a valid email';
-  if (!this.password.value) return 'Enter password';
-  if (this.password.value.trim().replace(/\s+/g, '').length < 8) return 'Password should contain at least 8 characters';
+  if (!(/.+@[^@]+\.[^@]{2,}$/.test(this.email.value))) {
+   return 'Enter a valid email';
+  }
+  if (!this.password.value) {
+   return 'Enter password';
+  }
+  if (this.password.value.trim().replace(/\s+/g, '').length < 8) {
+   return 'Password should contain at least 8 characters';
+  }
   return;
  }
  connectUser(data) {
-  console.log(data);
-  if (data.status === 201) this.redirect(data);
-  else {
+  if (data.status === 201) {
+   this.redirect(data);
+   return;
+  }
    this.box.classList.add('alert-danger');
    this.box.innerHTML = `<i class='icofont-error'><i> ${data.error || data.msg}`;
-   this.btn.disabled = true;
+   this.btn.disabled = false;
    this.btn.textContent = 'Create';
    this.btn.style.opacity = '1';
-  }
   return;
  }
  redirect(data) {
   this.box.classList.add('alert-success');
   this.box.innerHTML = `<i class = 'icofont-success'></i> Created User Redirecting...`;
   localStorage.setItem('user', JSON.stringify(data.data));
-  setTimeout(() => { location.replace('profile.html'); }, 3000);
+  setTimeout(() => { location.replace('profile.html'); }, 5000);
   return;
  }
 }
