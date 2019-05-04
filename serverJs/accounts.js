f415b6b1854b80dbf3a346e54d08e26632fca914
codexempire <princewillifeanyi1999@gmail.com>
2019-05-04 20:33:32 +0100

    feature(UI&API) linked the  debit and credit accounts linked the debit and credit account forms to he API displayed simple descriptive messages to inform the cashier if the account if account exists or if the transaction was succssfull [finishes #165559817]

--- serverJs/accounts.js
+++ serverJs/accounts.js
@@ -20,9 +20,8 @@ class Transaction {
   fetch(`${endpoint}/api/v1/accounts/${accountNumber}`, options)
    .then(res => res.json())
    .then(res => {
-    console.log(res);
     if (res.status === 200) {
-     this.balance+=`${res.data.balance}`;
+     this.balance.value = `${res.data.balance}`;
      return;
     }
     this.box.classList.add('alert-danger');
@@ -42,7 +41,86 @@ class Credit extends Transaction {
  constructor() {
   super();
  }
+ creditAccount() {
+  const accountNumber = document.querySelector('#accountNumber').value;
+  const endpoint = 'https://banka-pro-app.herokuapp.com';
+  const data = {
+   amount: parseInt(this.amount.value, 10)
+  };
+  const options = {
+   method: 'POST',
+   body: JSON.stringify(data),
+   headers: new Headers({
+    'Content-Type': 'application/json',
+    'x-access-token': `${this.user.token}`
+   })
+  };
+  fetch(`${endpoint}/api/v1/transactions/${accountNumber}/credit`, options)
+   .then(res => res.json())
+   .then(res => {
+    if (res.status === 403) {
+     location.replace('../login.html');
+     return;
+    }
+    if (res.status === 200) {
+     this.box.classList.add('alert-success');
+     this.box.textContent = `Credited ${res.data.accountnumber} with ${res.data.amount}`;
+     return;     
+    }
+    this.box.classList.add('alert-danger');
+    this.box.textContent = `${res.error}`;
+    return;
+   })
+   .catch(err => {
+    this.box.classList.add('alert-danger');
+    this.box.textContent = `${err.message}`;
+    return;
+   });
+  return;  
+ }
 }
 
+class Debit extends Transaction {
+ constructor() {
+  super();
+ }
+ debitAccount() {
+  const accountNumber = document.querySelector('#accountNumber').value;
+  const endpoint = 'https://banka-pro-app.herokuapp.com';
+  const data = {
+   amount: parseInt(this.amount.value, 10)
+  };
+  const options = {
+   method: 'POST',
+   body: JSON.stringify(data),
+   headers: new Headers({
+    'Content-Type': 'application/json',
+    'x-access-token': `${this.user.token}`
+   })
+  };
+  fetch(`${endpoint}/api/v1/transactions/${accountNumber}/debit`, options)
+   .then(res => res.json())
+   .then(res => {
+    if (res.status === 403) {
+     location.replace('../login.html');
+     return;
+    }
+    if (res.status === 200) {
+     this.box.classList.add('alert-success');
+     this.box.textContent = `Debited ${res.data.accountnumber} with ${res.data.amount}`;
+     return;
+    }
+    this.box.classList.add('alert-danger');
+    this.box.textContent = `${res.error}`;
+    return;
+   })
+   .catch(err => {
+    this.box.classList.add('alert-danger');
+    this.box.textContent = `${err.message}`;
+    return;
+   });
+  return;
+ }
+}
 // const credit = new Credit();
 // console.log(credit.getAccount());
