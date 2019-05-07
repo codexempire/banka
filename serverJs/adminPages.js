74f8badcfb49fa4723c5bef675b98ccbf603debc
codexempire <princewillifeanyi1999@gmail.com>
2019-05-07 19:52:46 +0100

    fixed he problem in the serverJs files

--- serverJs/adminPages.js
+++ serverJs/adminPages.js
@@ -375,6 +375,70 @@ const deleteAccount = (accountNumber) => {
     });
   return;
 }
+const viewTransaction = (id) => {
+  document.querySelector('.form-head').textContent = '';
+  document.querySelector('.hover').innerHTML = '';
+  document.querySelector('.alert').textContent = '';
+  document.querySelector('.alert').classList.remove('alert-danger');
+  const endpoint = 'https://banka-pro-app.herokuapp.com';
+  const options = {
+    method: 'GET',
+    headers: new Headers({
+      'Content-Type': 'application/json',
+      'x-access-token': `${user.token}`
+    })
+  };
+  fetch(`${endpoint}/api/v1/transactions/${id}`, options)
+    .then(res => res.json())
+    .then(res => {
+      if (res.status === 200) {
+        const box = document.querySelector('.diva');
+        return box.innerHTML = `
+            <div class='form-card centee'>
+              <h2 class='text-center'>Transaction Details</h2>
+              <table>
+                <tr>
+                  <td>Account Number</td>
+                  <td>${res.data.accountnumber}</td>
+                </tr>
+                <tr>
+                  <td>Amount</td>
+                  <td>${res.data.amount}</td>
+                </tr>
+                <tr>
+                  <td>Cashier Id</td>
+                  <td>${res.data.cashierid}</td>
+                </tr>
+                <tr>
+                  <td>Old Balance</td>
+                  <td>${res.data.oldbalance}</td>
+                </tr>
+                <tr>
+                  <td>New Balance</td>
+                  <td>${res.data.newbalance}</td>
+                </tr>
+                <tr>
+                  <td>Type</td>
+                  <td>${res.data.type}</td>
+                </tr>
+                <tr>
+                  <td>Date</td>
+                  <td>${res.data.createdon.slice(0, 10)}</td>
+                </tr>
+              </table>
+            </div>
+          `;
+      }
+      document.querySelector('.alert').textContent = `${res.error}`;
+      return;
+    })
+    .catch(err => {
+      document.querySelector('.alert').classList.add('alert-danger');
+      document.querySelector('.alert').textContent = `${err.message}`;
+      return;
+    });
+  return;
+}
 
 const logout = () => {
   localStorage.removeItem('user');
